6c2b23bb6fbdccb9c34eef444782d7513ed7e366
shashikala133 <sshashikala1330@gmail.com>
2022-07-14 15:32:37 +0530

    Commit 14/07/22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    CarouselComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
